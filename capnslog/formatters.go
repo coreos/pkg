799221ec970b294467fbeae5e2ab1b39e8aaa2bf
Barak Michener <me@barakmich.com>
2015-04-28 14:33:58 -0400

    Separate flush, improve the default string logger to match standard log

--- capnslog/formatters.go
+++ capnslog/formatters.go
@@ -2,12 +2,15 @@ package capnslog
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"strings"
+	"time"
 )
 
 type Formatter interface {
 	Format(pkg string, level LogLevel, depth int, entries ...LogEntry)
+	Flush()
 }
 
 func NewStringFormatter(w io.Writer) *StringFormatter {
@@ -20,13 +23,23 @@ type StringFormatter struct {
 	w *bufio.Writer
 }
 
-func (s *StringFormatter) Format(pkg string, _ LogLevel, _ int, entries ...LogEntry) {
+func (s *StringFormatter) Format(pkg string, l LogLevel, i int, entries ...LogEntry) {
+	now := time.Now()
+	y, m, d := now.Date()
+	h, min, sec := now.Clock()
+	s.w.WriteString(fmt.Sprintf("%d/%02d/%d %02d:%02d:%02d ", y, m, d, h, min, sec))
+	s.writeEntries(pkg, l, i, entries...)
+}
+
+func (s *StringFormatter) writeEntries(pkg string, _ LogLevel, _ int, entries ...LogEntry) {
 	if pkg != "" {
-		s.w.WriteString(pkg + ":")
+		s.w.WriteString(pkg + ": ")
 	}
 	endsInNL := false
-	for _, v := range entries {
-		s.w.WriteByte(' ')
+	for i, v := range entries {
+		if i != 0 {
+			s.w.WriteByte(' ')
+		}
 		str := v.LogString()
 		endsInNL = strings.HasSuffix(str, "\n")
 		s.w.WriteString(str)
@@ -34,5 +47,9 @@ func (s *StringFormatter) Format(pkg string, _ LogLevel, _ int, entries ...LogEn
 	if !endsInNL {
 		s.w.WriteString("\n")
 	}
+	s.Flush()
+}
+
+func (s *StringFormatter) Flush() {
 	s.w.Flush()
 }
